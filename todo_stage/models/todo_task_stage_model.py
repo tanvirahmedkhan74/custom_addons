b562fb004e5a004e4d4eb2cf2d853b2a2a243969
tanvirahmedkhan74 <tanvirahmedkhan74@gmail.com>
2020-07-18 14:07:25 +0600

    Fields addded

--- todo_stage/models/todo_task_stage_model.py
+++ todo_stage/models/todo_task_stage_model.py
@@ -6,5 +6,27 @@ class TodoStage(models.Model):
     _description = 'To-do Stage'
     _order = 'sequence,name'
 
-    name = fields.Char('Name', translate=True)
+    # string fields
+    name = fields.Char("Name")
+    desc = fields.Text("Description")
+    state = fields.Selection(
+        [('draft', 'new'),
+         ('Open', 'Started'),
+         ('done', 'closed')], 'State')
+    docs = fields.Html('Documentation')
+
+    # numeric Field
     sequence = fields.Integer('Sequence')
+    perc_complete = fields.Float('% Complete', (3, 2))
+
+    # date field
+
+    date_effective = fields.Date("Effecvtive Date")
+    date_created = fields.Datetime(
+        'Create Date and Time',
+        default=lambda self: fields.Datetime.new()
+    )
+
+    # other
+    fold = fields.Boolean('Folded')
+    image = fields.Binary('Image')
